86236fead1c77cd297a7a2710c5299620d4348dd
khazeenab8 <khazeenab8@gmail.com>
2025-12-31 21:44:36 +0500

    Create page.tsx

---
+++ app/button/page.tsx
@@ -0,0 +1,22 @@
+export default function ButtonDocs() {
+  return (
+    <>
+      <h1>Button</h1>
+      <p>Buttons are used to trigger actions.</p>
+
+      <button
+        style={{
+          padding: "10px 16px",
+          background: "#2563eb",
+          color: "white",
+          borderRadius: "10px",
+          border: "none",
+          cursor: "pointer",
+          fontWeight: 600,
+        }}
+      >
+        Primary Button
+      </button>
+    </>
+  );
+}
